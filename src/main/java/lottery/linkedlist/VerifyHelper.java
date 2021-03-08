63093a2b0993a9e9c884f37e30ce0c21c639c1a3
leec929 <46377936+leec929@users.noreply.github.com>
2021-03-08 01:43:24 -0500

    Create VerifyHelper.java

---
+++ src/main/java/lottery/linkedlist/VerifyHelper.java
@@ -0,0 +1,9 @@
+package lottery.linkedlist;
+
+public class VerifyHelper {
+
+	public static void main(String[] args) { 
+		System.out.println("This class is solely to help other Verify files work.");
+		System.out.println("You do not need to run this class.");
+	}
+}
